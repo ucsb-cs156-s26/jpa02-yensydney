c5fcd75321c46ad7547c716280e4cbbe26609225
Nini Yen <yensydney@gmail.com>
2026-04-14 15:19:52 -0700

    ny - updated with actual github id

--- src/main/java/edu/ucsb/cs156/spring/hello/Developer.java
+++ src/main/java/edu/ucsb/cs156/spring/hello/Developer.java
@@ -25,8 +25,7 @@ public class Developer {
      */
 
     public static String getGithubId() {
-        // TODO: Change this to your github id
-        return "cgaucho";
+        return "Nini";
     }
 
     /**
--- src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
@@ -27,5 +27,8 @@ public class DeveloperTest {
 
     // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
     // 100% mutation coverage (all mutants timed out or killed)
-
+    @Test
+        public void getGithubId_returns_correct_githubId() {
+            assertEquals("Nini", Developer.getGithubId());
+        }
 }
